944bea49f9455e665559672ff3b284b4a1b06eb8
Ebrahim-Moqbel <ebra72himuni@gmail.com>
2026-07-28 02:07:01 +0100

    solving key exerciese 1

--- Sprint-1/1-key-exercises/3-paths.js
+++ Sprint-1/1-key-exercises/3-paths.js
@@ -17,7 +17,7 @@ console.log(`The base part of ${filePath} is ${base}`);
 // Create a variable to store the dir part of the filePath variable
 // Create a variable to store the ext part of the variable
 
-const dir = ;
-const ext = ;
+const dir = filepath.slice(0,lastSlashIndex);
+const ext = filepath.slice(filePath.lastIndexOf("."));
 
 // https://www.google.com/search?q=slice+mdn
