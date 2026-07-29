20c4e70ed1b4d676db1892646cb4347dedadd0ee
Ebrahim_Moqbel <ebra72himuni@gmail.com>
2026-07-29 01:31:38 +0100

    reversing the commits from the main stream to pass the meta data for sprint-1

--- Sprint-1/1-key-exercises/1-count.js
+++ Sprint-1/1-key-exercises/1-count.js
@@ -5,5 +5,4 @@ count = count + 1;
 // Line 1 is a variable declaration, creating the count variable with an initial value of 0
 // Describe what line 3 is doing, in particular focus on what = is doing
 
-// Line 3 is an increment  where = is an assignment operator and in there we are adding value of 1 to the count variable
 
--- Sprint-1/1-key-exercises/4-random.js
+++ Sprint-1/1-key-exercises/4-random.js
@@ -7,10 +7,3 @@ const num = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 // Try breaking down the expression and using documentation to explain what it means
 // It will help to think about the order in which expressions are evaluated
 // Try logging the value of num and running the program several times to build an idea of what the program is doing
-
-
-// math.floor will make the number as a whole and remove any decimals or round the number to the nearest whole number.
-// math.random returns a random number in (0,1] excluding the 1
-// (maximum - minimum +1) provide a range or generated random number where would more likely to be 100 in this case
-// num is a random whole number [0,100]
-console.log(num);
